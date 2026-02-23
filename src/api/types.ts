7babf02fb9f99dd8a525047b038277006bedbc62
ResourceBrain <nnaveenaj@gmail.com>
2026-02-23 11:22:25 +0530

    final  commit

--- src/api/types.ts
+++ src/api/types.ts
@@ -139,7 +139,11 @@ export interface User {
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
   role: 'guest' | 'institute' | 'admin' | 'teacher' | 'vendor' | 'marketing' | 'sales';
+=======
+  role: 'guest' | 'institute' | 'admin' | 'teacher' | 'vendor' | 'marketing';
+>>>>>>> Stashed changes
 =======
   role: 'guest' | 'institute' | 'admin' | 'teacher' | 'vendor' | 'marketing';
 >>>>>>> Stashed changes
